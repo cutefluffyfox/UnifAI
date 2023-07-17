cf80450446efec50dfbb912e06892fbf3d15cf9f
geffk2 <sluxx@icloud.com>
2023-07-17 20:02:24 +0300

    Fixed a couple of mistakes

--- postgres/psql_dump.sql
+++ postgres/psql_dump.sql
@@ -3,14 +3,14 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"username" varchar UNIQUE,
 	"passhash" varchar,
 	"audio" bytea,
-	"last_update" timestamp)
+	"last_update" timestamp);
 
 CREATE TABLE IF NOT EXISTS "rooms" (
 	"id" serial PRIMARY KEY,
 	"admin_id" integer references users(id),
 	"name" varchar,
-	"description" varchar)
+	"description" varchar);
 
 CREATE TABLE IF NOT EXISTS "rooms_users" (
 	"user_id" integer references users(id),
-	"room_id" integer references rooms(id))
+	"room_id" integer references rooms(id));
